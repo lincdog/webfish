9a6e0b110da40b50325f7bc2272052afea7db0ba
Lincoln Ombelets <lombelets@caltech.edu>
2021-03-22 18:20:32 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,74 @@
+import json
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+import numpy as np
+
+
+MESH_FILE = 'labeled_16x_mesh.json'
+PCD_FILE = 'dots_colored.json'
+
+with open(MESH_FILE, 'r') as mf:
+    cell_mesh = json.load(mf)
+
+with open(PCD_FILE, 'r') as pf:
+    dots_pcd = json.load(pf)
+
+cell_mesh['verts'] = np.array(cell_mesh['verts'])
+cell_mesh['faces'] = np.array(cell_mesh['faces'])
+
+dots_pcd['points'] = np.array(dots_pcd['points'])
+
+z,y,x = cell_mesh['verts'].T
+i,j,k = cell_mesh['faces'].T
+
+pz,py,px = dots_pcd['points'].T
+
+figdata = [
+    go.Mesh3d(x=x, y=y, z=z, 
+              i=i, j=j, k=k,
+        color='lightpink',
+        opacity=1,
+        hoverinfo='skip',
+    ),
+    go.Scatter3d(x=px, y=py, z=pz,
+        mode='markers',
+        marker=dict(
+            size=1,
+            color=dots_pcd['colors'],
+            opacity=1,
+            symbol='circle',
+        ),
+        hoverinfo='skip',
+    )
+]
+
+figscene = go.layout.Scene(
+    aspectmode='data'
+)
+
+figlayout= go.Layout(
+    height=800,
+    width=800,
+    margin=dict(b=10, l=10, r=10, t=10),
+    scene=figscene
+)
+
+
+fig = go.Figure(data=figdata, layout=figlayout)
+#fig.show(renderer='jupyterlab')
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Tester'),
+    dcc.Graph(
+        id='test-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
