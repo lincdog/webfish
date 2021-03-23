20728a87a15a17124bf6fb255b63677572d9dd46
Lincoln Ombelets <lombelets@caltech.edu>
2021-03-22 22:41:51 -0700

    gene selector start

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
@@ -22,7 +23,13 @@ cell_mesh['faces'] = np.array(cell_mesh['faces'])
 z,y,x = cell_mesh['verts'].T
 i,j,k = cell_mesh['faces'].T
 
-pz,py,px = dots_pcd[['z', 'y', 'x']].values.T
+unique_genes, gene_counts = np.unique(dots_pcd['geneID'], return_counts=True)
+
+possible_genes = ['All'] + list(np.flip(unique_genes[np.argsort(gene_counts)]))
+
+
+############# Begin app code
+app = dash.Dash(__name__)
 
 figdata = [
     go.Mesh3d(x=x, y=y, z=z,
@@ -31,16 +38,8 @@ figdata = [
         opacity=0.7,
         hoverinfo='skip',
     ),
-    go.Scatter3d(x=px, y=py, z=pz,
-        mode='markers',
-        marker=dict(
-            size=1,
-            color=dots_pcd['geneColor'],
-            opacity=1,
-            symbol='circle',
-        ),
-        hoverinfo='text',
-        hovertext=dots_pcd['geneID']
+    go.Scatter3d(
+        name='dots',
     )
 ]
 
@@ -60,18 +59,62 @@ figlayout= go.Layout(
 
 
 fig = go.Figure(data=figdata, layout=figlayout)
-#fig.show(renderer='jupyterlab')
 
 
-app = dash.Dash(__name__)
+@app.callback(
+    Output('test-graph', 'figure'),
+    [Input('gene-select', 'value'),
+     Input('test-graph', 'figure')
+    ])
+def update_figure(selected_genes, fig):
+    
+    fig = go.Figure(fig)
+    
+    if 'All' in selected_genes:
+        dots_filt = dots_pcd
+    else:
+        dots_filt = dots_pcd.query('geneID in @selected_genes')
+
+    pz,py,px = dots_filt[['z', 'y', 'x']].values.T
+
+    newscatter_props = dict(
+        x=px, y=py, z=pz,
+        mode='markers',
+        marker=dict(
+            size=2,
+            color=dots_filt['geneColor'],
+            opacity=1,
+            symbol='circle',
+        ),
+        hoverinfo='text',
+        hovertext=dots_filt['geneID']
+    )
+    
+
+    newfig = fig.update_traces(newscatter_props, {'name': 'dots'})
+    #fig.show(renderer='jupyterlab')
+
+    return newfig
+
 
 app.layout = html.Div(children=[
-    html.H1(children='Tester', style={'font-family': 'sans-serif'}),
-    dcc.Graph(
-        id='test-graph',
-        figure=fig
-    )
-], style={'align': 'center'})
+    html.H1(children='webfish test', style={'font-family': 'sans-serif', 'margin': 'auto'}),
+    
+    html.Div([
+            dcc.Dropdown(
+                id='gene-select',
+                options=[{'label': i, 'value': i} for i in possible_genes],
+                value='',
+                multi=True
+            ),
+    ], style=dict(width='200px')),  
+    html.Div([
+        dcc.Graph(
+            id='test-graph',
+            figure=fig
+        )
+    ]),
+], style={'margin': 'auto'})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
