e8055e3b3c6b8e3ce7b08ed3647489440cf21eec
Lincoln Ombelets <lombelets@caltech.edu>
2021-03-29 22:28:10 -0700

    basic cloud-init script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-pip install -r requirements.txt
+#cloud-config
+repo_upgrade: security
+
+packages:
+ - git
+
+runcmd:
+ - [ git, clone, "https://github.com/lincdog/webfish.git" ]
+ - cd webfish
+ - [ pip, install, -r, requirements.txt ]
