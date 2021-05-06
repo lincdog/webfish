4483e144027ddf0ce80f48624b5d766d27daae64
Lincoln Ombelets <lombelets@caltech.edu>
2021-05-06 13:45:42 -0700

    client/server qcli test

--- scripts/qcli.py
+++ scripts/qcli.py
@@ -2,10 +2,10 @@ from importlib import reload
 
 import yaml
 import sys
-sys.path.append('..')
 import lib.cloud as cloud
 
-def qtest(pagename='datavis'):
+
+def qclient(pagename='datavis'):
     global cloud
     cloud = reload(cloud)
 
@@ -15,3 +15,13 @@ def qtest(pagename='datavis'):
 
     return config, s3c, client
 
+
+def qserver():
+    global cloud
+    cloud = reload(cloud)
+
+    config = yaml.load(open('../consts.yml'), Loader=yaml.Loader)
+    s3c = cloud.S3Connect(config=config)
+    server = cloud.DataServer(config=config, s3_client=s3c)
+
+    return config, s3c, server
