de32fd7779389cc3d071604efee5fd2eae430d47
Lincoln Ombelets <lombelets@caltech.edu>
2021-05-14 01:00:06 -0700

    atexit to release lock on err

--- scripts/upload_datasets.py
+++ scripts/upload_datasets.py
@@ -2,9 +2,8 @@ import os
 import sys
 import yaml
 import time
-import signal
-import pandas as pd
 import logging
+import atexit
 from logging.handlers import RotatingFileHandler
 from pathlib import Path, PurePath
 from argparse import ArgumentParser
@@ -74,42 +73,6 @@ def stat_compare(dm):
     return source_modified, raw_modified, listmtime
 
 
-def dataset_compare(dm):
-    old_datasets = pd.read_csv(Path(dm.sync_folder, 'all_datasets.csv'), dtype=str)
-
-    new_datasets = dm.get_datasets()
-
-    diff = set(old_datasets['folder']) ^ set(new_datasets['folder'])
-
-    return list(diff)
-
-
-def sigint_write_pending(signo, frame):
-    pending_csv = frame.f_globals.get('pending_csv', None)
-    df = frame.f_globals.get('remaining_files', None)
-
-    lockfile = frame.f_globals.get('lock', None)
-
-    if pending_csv and df:
-        try:
-            df.to_csv(pending_csv, index=False)
-            print('Keyboard interrupt received, saving pending uploads and exiting.')
-            sys.exit(0)
-        except IOError:
-            print('Keyboard interrupt received, failed to save pending uploads.')
-            sys.exit(1)
-
-    if lockfile:
-        try:
-            lockfile.unlink(missing_ok=True)
-            sys.exit(0)
-        except AttributeError:
-            print('Keyboard interrupt received but failed to unlink lockfile.')
-            sys.exit(1)
-
-    sys.exit(1)
-
-
 def search_and_upload(dm, mtime, use_s3_only=False, check_s3=False, dryrun=False):
 
     results = {}
@@ -124,8 +87,6 @@ def search_and_upload(dm, mtime, use_s3_only=False, check_s3=False, dryrun=False
     # read in the s3 keys, unless --check-s3 is specified, from the local
     # cached listing.
     dm.check_s3_contents(use_local=(not check_s3))
-
-    signal.signal(signal.SIGINT, sigint_write_pending)
 
     for pagename in dm.pagenames:
         pending, uploaded = dm.upload_to_s3(
@@ -145,6 +106,10 @@ def search_and_upload(dm, mtime, use_s3_only=False, check_s3=False, dryrun=False
     return results
 
 
+def atexit_release(file):
+    file.unlink(missing_ok=True)
+
+
 def main(args):
 
     dm = init_server()
@@ -158,6 +123,8 @@ def main(args):
 
     with open(lock, 'w') as lockfp:
         lockfp.write('{0} {1}'.format(os.getpid(), time.time()))
+
+    atexit.register(atexit_release, lock)
 
     if args.fresh:
         mtime = 0
