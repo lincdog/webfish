99fa42d75e0f160f854edf347456ad1e6ca67a91
Lincoln Ombelets <lombelets@caltech.edu>
2021-07-06 12:42:20 -0700

    add requirements specification to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('./README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
+with open('./requirements.txt', 'r') as rfh:
+    lib_flag = False
+    requirements = []
+
+    for line in rfh.readlines():
+        if line.startswith('#') and 'lib' in line:
+            lib_flag = True
+        elif lib_flag and line:
+            requirements.append(line)
+        elif not line:
+            break
+
 setuptools.setup(
-    name="webfish-tools",
-    version="0.0.1",
-    author="Lincoln Ombelets",
-    author_email="lombelets@caltech.edu",
-    description="The utilities and backend for the webfish UI",
+    name='webfish-tools',
+    version='0.0.1',
+    author='Lincoln Ombelets',
+    author_email='lombelets@caltech.edu',
+    description='The utilities and backend for the webfish UI',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/CaiGroup/web-ui",
+    long_description_content_type='text/markdown',
+    url='https://github.com/CaiGroup/web-ui',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3',
     ],
-    package_dir={"": "lib"},
-    packages=setuptools.find_packages(where="lib"),
-    python_requires=">=3.9",
+    package_dir={'': 'lib'},
+    packages=setuptools.find_packages(where='lib'),
+    python_requires='>=3.9',
+    install_requires=requirements
 )
